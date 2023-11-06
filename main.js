8a497bda2da4a4f566ef8a5b11e79243eaa35084
Marcos <marcospadilhapires@gmail.com>
2023-11-06 11:36:54 -0300

    Usado o for,adicionado clique com o teclado.

--- main.js
+++ main.js
@@ -4,15 +4,23 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while(contador < listaDeTeclas.length){
+for(let contador = 0; contador < listaDeTeclas.length;contador++)
+{
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1]
-    //template string
-    const idAudio = `#som_${instrumento}`
-    listaDeTeclas[contador].onclick = function(){
+    const idAudio = `#som_${instrumento}` //template string
+
+    tecla.onclick = function()
+    {
         tocaSom(idAudio)
-    };
-    contador = contador+1;
+    }
+    tecla.onkeydown = function(evento){
+        if(evento.code === "Enter" || evento.code === "Space")
+        {
+            tecla.classList.add('ativa');
+        }  
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
